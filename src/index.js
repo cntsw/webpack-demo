f98761c01ab785c795e329d08bdcb5296390e02d
tswgit <dztxd@126.com>
2019-04-20 15:31:10 +0800

    update xterm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
 
 import { Terminal } from 'xterm';
+import * as fullscreen from 'xterm/lib/addons/fullscreen/fullscreen';
+import * as fit from 'xterm/lib/addons/fit/fit';
+
 import React from 'react';
 import {
   render,
@@ -8,10 +11,35 @@ import {
 import App from './App';
 
 import './index.css';
+import 'xterm/dist/addons/fullscreen/fullscreen.css'
 import 'xterm/dist/xterm.css';
 
-const term = new Terminal();
-term.open(document.getElementById('xterm'));
+// render(<App />, document.getElementById('root'));
 
 
-render(<App />, document.getElementById('root'));
+// https://xtermjs.org/docs/api/addons/fullscreen/
+Terminal.applyAddon(fullscreen);
+
+// https://xtermjs.org/docs/api/addons/fit/
+// Terminal.applyAddon(fit);
+
+const term = new Terminal({
+  cursorBlink: true,
+  useStyle: true,
+  fontSize: 14,
+});
+term.open(document.getElementById('container-shell'));
+
+var ws = new WebSocket("ws://localhost:8080/xterm")
+ws.onmessage = function (message) {
+  term.write(window.atob(message.data));
+}
+term.on('data', function (data) {
+  ws.send(window.btoa(data))
+});
+
+window.term = term
+term.toggleFullScreen(true);
+// term.fit();
+term.focus();
+
