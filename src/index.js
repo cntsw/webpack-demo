36bec37e7791a4ec747d5fe8761012ee11f11986
tswgit <dztxd@126.com>
2019-04-20 12:55:44 +0800

    add xtermjs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
+
+
+import { Terminal } from 'xterm';
 import React from 'react';
 import {
   render,
 } from 'react-dom';
 import App from './App';
+
 import './index.css';
+import 'xterm/dist/xterm.css';
+
+const term = new Terminal();
+term.open(document.getElementById('xterm'));
+
 
 render(<App />, document.getElementById('root'));
